dd0a96c50bda1625d1fd49a2b8bb247d31e3acf4
Carlos Rodriguez <crodriguezyt10@gmail.com>
2023-08-24 11:14:15 -0700

    feat: metrics endpoint works and gets country data

--- src/pages/api/metrics.tsx
+++ src/pages/api/metrics.tsx
@@ -1,19 +1,39 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import connectMongoDB from '@/lib/MongoConnect';
+import Event from '@/models/EmailEventSchema';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    try {
+      await connectMongoDB();
 
+      // Retrieve events from the database
+      const events = await Event.find();
 
-        
-      // aggregate events data and calculate metrics
+      // Iterate through events and calculate metrics
+      let countryCount:any = {};
 
+      events.forEach(event => {
+        // Check if the event has geo_ip and country attributes
+        if (event.geo_ip && event.geo_ip.country) {
+          const country = event.geo_ip.country;
 
+          // Count and accumulate the country occurrences
+          if (countryCount[country]) {
+            countryCount[country]++;
+          } else {
+            countryCount[country] = 1;
+          }
+        }
+      });
+      console.log(countryCount)
 
-
-      const avgTimePerEmail = 'placeholder';
-      res.json({ avgTimePerEmail });
-    } else {
-      res.status(405).json({ message: 'Method not allowed' });
+      res.status(200).json({ countryCount });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Error fetching data from the database' });
     }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
   }
-  
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,41 @@
 import axios from "axios";
-import data from "../../lib/testEvent"
-;
-const handleClick = async () => {
-  const sendData = await axios.post('/api/events', {event: data})
-}
+import data from "../../lib/testEvent";
 
 export default function Home() {
-    return (
-      <div>
-        Hi
-        <button onClick={handleClick}>
-          Click here 
-        </button>
-      </div>
-    );
-  }
+  const handlePostEventClick = async () => {
+
+    try {
+      await axios.post('/api/events', { event: data });
+      console.log('Event posted successfully');
+    } catch (error) {
+      console.error('Error posting event:', error);
+    }
+  };
+
+  const handleGetMetricsClick = async () => {
+    try {
+      const response = await axios.get('/api/metrics');
+      const metrics = response.data;
+      console.log('Metrics:', metrics);
+    } catch (error) {
+      console.error('Error getting metrics:', error);
+    }
+  };
+
+  return (
+    <div>
+      <button
+        onClick={handlePostEventClick}
+        className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full shadow-md'
+      >
+        POST Event
+      </button>
+      <button
+        onClick={handleGetMetricsClick}
+        className='bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full shadow-md'
+      >
+        GET Metrics
+      </button>
+    </div>
+  );
+}
