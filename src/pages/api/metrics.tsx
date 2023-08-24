faf8ace8a8c65b8051d0245614c5b5b24abba5dc
Carlos Rodriguez <crodriguezyt10@gmail.com>
2023-08-24 12:53:52 -0700

    feat: GET metrics endpoint now returns device count and country count

--- src/pages/api/metrics.tsx
+++ src/pages/api/metrics.tsx
@@ -11,12 +11,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const events = await Event.find();
 
       // Iterate through events and calculate metrics
-      let countryCount:any = {};
+      let countryCount:any = {US: 0, ES: 0, IT: 0};
+      let deviceCount: any = {desktop: 0, mobile: 0, tablet: 0};
+      let timeseries:any = [];
 
       events.forEach(event => {
         // Check if the event has geo_ip and country attributes
-        if (event.geo_ip && event.geo_ip.country) {
+        if (event.geo_ip && event.geo_ip.country && event.user_agent_parsed) {
           const country = event.geo_ip.country;
+          const device = event.user_agent_parsed.is_mobile;
 
           // Count and accumulate the country occurrences
           if (countryCount[country]) {
@@ -24,11 +27,24 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           } else {
             countryCount[country] = 1;
           }
+
+          // Count and accumulate the device occurrences
+          if (device) {
+            deviceCount["mobile"]++;
+          } else{
+            // Other functions to determine if its a desktop or tablet
+          }
+
+
+
+
+
         }
       });
       console.log(countryCount)
+      console.log(deviceCount)
 
-      res.status(200).json({ countryCount });
+      res.status(200).json({ countryCount, deviceCount });
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: 'Error fetching data from the database' });
