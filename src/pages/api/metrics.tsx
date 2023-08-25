804a295a62d8d955fc6ebfa1f7e5fa0702a8712b
Carlos Rodriguez <crodriguezyt10@gmail.com>
2023-08-24 20:57:50 -0700

    feat: added features to allow user to see backend data be processed on the frontend

--- src/pages/api/metrics.tsx
+++ src/pages/api/metrics.tsx
@@ -67,7 +67,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       console.log(deviceCount);
       console.log(timeseries);
 
-      res.status(200).json({ countryCount, deviceCount });
+      res.status(200).json({ countryCount, deviceCount, timeseries });
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: 'Error fetching data from the database' });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,104 @@
-import axios from "axios";
-import data from "../../lib/testEvent";
+import React, { useState } from 'react';
+import axios from 'axios';
+import data from '../../lib/testEvent';
+
+interface TimeseriesData {
+  [timestamp: string]: {
+    totalOpens: number;
+    time: string;
+  };
+}
+interface MetricsData {
+  countryCount: any;
+  deviceCount: any;
+  timeseries: TimeseriesData;
+}
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [metricsData, setMetricsData] = useState<MetricsData | null>(null);
+
   const handlePostEventClick = async () => {
+    if (isLoading) return; // Don't allow button press if loading is in progress
+
+    setIsLoading(true); // Start loading
+
     // Update the timestamp to the current time before sending
     const eventData = {
       ...data,
       timestamp: new Date().toISOString()
-    };    
+    };
+
     try {
       await axios.post('/api/events', { event: eventData });
       console.log('Event posted successfully');
     } catch (error) {
       console.error('Error posting event:', error);
+    } finally {
+      setIsLoading(false); // Stop loading
+      handleGetMetricsClick()
     }
   };
 
   const handleGetMetricsClick = async () => {
+    if (isLoading) return; // Don't allow button press if loading is in progress
+
+    setIsLoading(true); // Start loading
+
     try {
       const response = await axios.get('/api/metrics');
       const metrics = response.data;
       console.log('Metrics:', metrics);
+      setMetricsData(metrics);
     } catch (error) {
       console.error('Error getting metrics:', error);
+    } finally {
+      setIsLoading(false); // Stop loading
     }
   };
+
+
 
   return (
     <div>
       <button
         onClick={handlePostEventClick}
-        className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full shadow-md'
+        className={`${
+          isLoading ? 'opacity-50 cursor-not-allowed' : ''
+        } bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full shadow-md`}
       >
         POST Event
       </button>
       <button
         onClick={handleGetMetricsClick}
-        className='bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full shadow-md'
+        className={`${
+          isLoading ? 'opacity-50 cursor-not-allowed' : ''
+        } bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full shadow-md`}
       >
         GET Metrics
       </button>
+      {/* Display metrics data if available */}
+      {metricsData && (
+        <div>
+          <h2>Metrics Data:</h2>
+          <p>Country Count: {JSON.stringify(metricsData.countryCount)}</p>
+          <p>Device Count: {JSON.stringify(metricsData.deviceCount)}</p>
+          {Object.keys(metricsData.timeseries).length > 0 ? (
+            <div>
+              <p>Timeseries:</p>
+              <ul>
+                {Object.keys(metricsData.timeseries).reverse().map((timestamp:any, index) => (
+                  <li key={index}>
+                    {`Time: ${metricsData.timeseries[timestamp].time}, Total Opens: ${metricsData.timeseries[timestamp].totalOpens}`}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <p>No timeseries data available.</p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
