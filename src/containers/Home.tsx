35e47aa263970ee4ab62a960f4f9f6efad810beb
Carlos Rodriguez <crodriguezyt10@gmail.com>
2023-09-15 11:56:48 -0700

    Organized where ChatBox component is located, removed unnecc code.

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,11 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { useUser } from '@auth0/nextjs-auth0/client';
-import ChatBox from '../components/home/ChatBox';
+import ChatBox from '../components/ChatBox/ChatBox';
 
 function HomeContainer() {
-  const { user, error } = useUser();
-  const [isLoading, setIsLoading] = useState(false);
-  const [userInput, setUserInput] = useState('');
+
 
   return (
     <div className="Home-Container">
